a97d8d6ea8acde4eb0b9e6ddaf3c7a2445beebce
unknown <hank@bankpro.com.tw>
2016-03-29 22:34:30 +0800

    test version

---
+++ src/nerby/action/indexaction2.java
@@ -0,0 +1,14 @@
+package nerby.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class indexaction2 extends ActionSupport{
+
+
+
+	    public String aaa() {
+	        return "success";
+	    }
+
+	
+}
